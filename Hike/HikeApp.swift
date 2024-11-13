c38dc8ea8cf2f3d873de2bb04a21fb3eb1cb0798
Aman <Aman@A1apps.co>
2024-11-13 18:24:28 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Aman on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
