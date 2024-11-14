6df0db63bd540796a8a1e21336ece0724f5c5e6b
Aman <Aman@A1apps.co>
2024-11-14 10:59:17 +0530

    Displayed List items.

--- Hike/ContentView.swift
+++ Hike/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let hikes = [
+        Hike(name: "Valley of Flowers Trek", photo: "valley_of_flowers", distance: 8),
+        Hike(name: "Roopkund Trek", photo: "roopkund", distance: 15),
+        Hike(name: "Har Ki Dun Trek", photo: "har_ki_dun", distance: 12)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            List(hikes) { hike in
+                HStack{
+                    Image(hike.photo).resizable().frame(width: 100, height: 60).clipShape(RoundedRectangle(cornerRadius: 6))
+                    VStack(alignment: .leading){
+                        Text(hike.name).padding(.bottom, 2)
+                        Text("\(hike.distance) km")
+                    }.padding(.leading, 10)
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ Hike/Hike.swift
@@ -0,0 +1,15 @@
+//
+//  Hike.swift
+//  Hike
+//
+//  Created by Aman on 13/11/24.
+//
+
+import Foundation
+
+struct Hike : Identifiable{
+    let id = UUID()
+    let name: String
+    let photo: String
+    let distance: Int
+}
