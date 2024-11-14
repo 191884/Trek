7d0b9e66db67e7d8d54962d2334486603d4d8830
Aman <Aman@A1apps.co>
2024-11-14 13:51:00 +0530

    Final Build for Hike(Added Navigation).

---
+++ Hike/TrekDetailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TrekDetailScreen.swift
+//  Hike
+//
+//  Created by Aman on 14/11/24.
+//
+
+import SwiftUI
+
+struct TrekDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrekDetailScreen()
+}
